bc789290a09b8cfb3214b70db59843d7f59f9186
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2025-06-08 00:06:59 +0300

    first commit

---
+++ src/main/java/com/jgitundu8/ai_review_agent/AiReviewAgentApplication.java
@@ -0,0 +1,13 @@
+package com.jgitundu8.ai_review_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiReviewAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiReviewAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgitundu8/ai_review_agent/AiReviewAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgitundu8.ai_review_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiReviewAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
