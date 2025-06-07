f43b517f3e9970de777efcaa5f7e3e261dca7d0f
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2025-06-08 00:10:31 +0300

    Test: Add ExampleService.java for PR analysis

---
+++ src/main/java/com/jgitundu8/ai_review_agent/ExampleService.java
@@ -0,0 +1,11 @@
+package com.jgitundu8.ai_review_agent;
+
+/**
+ * @author Jackson G. Mwaniki
+ */
+public class ExampleService {
+    public void doSomething() {
+        // TODO: Add authentication check
+        System.out.println("Doing something risky...");
+    }
+}
